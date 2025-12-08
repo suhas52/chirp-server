e2212a3074d20a291e672bca29dc6ae86c620d2c
suhas52 <suhasedu991@gmail.com>
2025-12-08 14:12:26 +0530

    custom zod error for the user routes

--- server/src/zodSchemas/userSchemas.ts
+++ server/src/zodSchemas/userSchemas.ts
@@ -1,5 +1,7 @@
-import * as zod from "zod"; 
+import * as zod from "zod";
+
+const postField = zod.string().min(3, "Your content should atleast 3 letters long").max(255, "Your post cannot be more than 255 letters long")
 
 export const postSchema = zod.object({
-    content: zod.string().min(3).max(255)
+    content: postField
 })

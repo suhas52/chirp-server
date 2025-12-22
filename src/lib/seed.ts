e3f9b33feaef82377ef3edf725f21b5a6acc89d2
suhas52 <suhasedu991@gmail.com>
2025-12-22 12:03:57 +0530

    replace manual seed data generation with faker

---
+++ src/lib/seed.ts
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker'
+import envConf from './envConfig.ts';
+import bcrypt from 'bcryptjs';
+
+
+const SALT = envConf.SALT;
+
+export async function createRandomUser() {
+    return {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        username: faker.internet.username(),
+        passwordHash: await bcrypt.hash("Test@123", SALT),
+        bio: faker.person.bio()
+    }
+}
+
+export function createPost() {
+    return faker.lorem.lines(3)
+
+}
--- src/routes/seedRoutes.ts
+++ src/routes/seedRoutes.ts
@@ -1,63 +1,71 @@
 import { Router, type NextFunction, type Request, type Response } from "express";
-import envConf from "../lib/envConfig.ts";
-import { registerSchema } from "../zodSchemas/authSchemas.ts";
 import { prisma } from '../lib/prismaConfig.ts';
-import bcrypt from 'bcryptjs';
 import { successResponse } from "../lib/response.ts";
+import { createPost, createRandomUser } from "../lib/seed.ts";
 import { CustomError } from "../lib/customError.ts";
 
-
-const SALT = envConf.SALT;
-
-
-function generateFastRandomString(length: number) {
-    return Math.random().toString(36).substring(2, length + 2);
-}
-
-interface NewPostData {
-    userId: String,
-    content: String
-}
 
 export const seedRouter = Router();
 
 seedRouter.post("/register/:userNumber", async (req: Request, res: Response) => {
     const userNumber = Number(req.params['userNumber']);
+
     let users = [];
     for (let i = 1; i <= userNumber; i++) {
-        const newUserData = {
-            firstName: generateFastRandomString(5),
-            lastName: generateFastRandomString(5),
-            username: generateFastRandomString(8),
-            passwordHash: await bcrypt.hash("Test@123", SALT),
-            bio: generateFastRandomString(8)
-        }
-
+        const fakerData = createRandomUser();
         const newUser = await prisma.user.create({
-            data: newUserData
+            data: await fakerData
         })
         users.push(newUser)
     }
     return successResponse(res, 200, { users })
 })
 
-// seedRouter.post("/post/:postNumber", async (req: Request, res: Response, next: NextFunction) => {
-//     const postNumber = Number(req.params['postNumber'])
-//     const userIds = await prisma.user.findMany({
-//         select: { id: true }
-//     })
-//     if (!userIds) return next(new CustomError("Unable to seed", 400))
-//     const posts = [];
-//     for (let i = 1; i <= postNumber; i++) {
-//         let randomArrayIndex = Math.floor(Math.random() * userIds.length);
-//         const newPostData: NewPostData = {
-//             userId: userIds[randomArrayIndex].id,
-//             content: generateFastRandomString(40)
-//         }
-//         const newPost = await prisma.post.create({
-//             data: newPostData
-//         })
-//         posts.push(newPost)
-//     }
-//     return successResponse(res, 200, posts)
-// })
+seedRouter.post("/posts/:postNumber", async (req: Request, res: Response, next: NextFunction) => {
+    const postNumber = Number(req.params['postNumber'])
+    const userIds = await prisma.user.findMany({
+        select: { id: true }
+    })
+    if (userIds.length === 0) {
+        return next(new CustomError("No users found", 400));
+    }
+
+    const posts = [];
+    for (let i = 1; i <= postNumber; i++) {
+        let randomArrayIndex = Math.floor(Math.random() * userIds.length);
+
+        const newPost = await prisma.post.create({
+            data: {
+                userId: userIds[randomArrayIndex]!.id,
+                content: createPost()
+            }
+        })
+        posts.push(newPost)
+    }
+    return successResponse(res, 200, posts)
+})
+
+seedRouter.post("/comments/:commentNumber", async (req: Request, res: Response, next: NextFunction) => {
+    const commentNumber = Number(req.params['commentNumber'])
+    const postIds = await prisma.post.findMany({
+        select: { id: true }
+    })
+    const userIds = await prisma.user.findMany({
+        select: { id: true }
+    })
+    if (!postIds || !userIds) return next(new CustomError("Unable to seet", 400))
+    const comments = [];
+    for (let i = 1; i <= commentNumber; i++) {
+        let randomPostArrayIndex = Math.floor(Math.random() * postIds.length)
+        let randomUserArrayIndex = Math.floor(Math.random() * userIds.length);
+        const newComment = await prisma.comment.create({
+            data: {
+                postId: postIds[randomPostArrayIndex]!.id,
+                userId: userIds[randomUserArrayIndex]!.id,
+                content: createPost()
+            }
+        })
+        comments.push(newComment)
+    }
+    return successResponse(res, 200, comments)
+})
